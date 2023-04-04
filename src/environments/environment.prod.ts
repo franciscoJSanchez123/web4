0cc2768308284afafe46ab333c3bfda002e79551
franciscoJSanchez123 <javisan0105@gmail.com>
2023-04-04 17:18:47 -0430

    import firebase modules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyDpqr_0RZbExKRWyhcL_vE1vFgT_qBbpKA",
+    authDomain: "web1-fd135.firebaseapp.com",
+    projectId: "web1-fd135",
+    storageBucket: "web1-fd135.appspot.com",
+    messagingSenderId: "193547943716",
+    appId: "1:193547943716:web:75b915aa04957fd8637db1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyDpqr_0RZbExKRWyhcL_vE1vFgT_qBbpKA",
+    authDomain: "web1-fd135.firebaseapp.com",
+    projectId: "web1-fd135",
+    storageBucket: "web1-fd135.appspot.com",
+    messagingSenderId: "193547943716",
+    appId: "1:193547943716:web:75b915aa04957fd8637db1"
+  }
 };
 
 /*
